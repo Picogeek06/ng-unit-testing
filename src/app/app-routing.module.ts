dadbfb95e4feda54e3c572c88687f979cabcd020
Picogeek06 <gopal.mishra25798@gmail.com>
2022-11-17 10:24:35 +0530

    Jest Setup & Basic Counter Component. 100% code covered.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CounterComponent } from './common/components/counter/counter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'counter',
+    component: CounterComponent,
+  },
+  {
+    path: '**',
+    component: CounterComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
